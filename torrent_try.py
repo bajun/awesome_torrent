12d8ff34e0d384618db865272a053ec0551b6ab1
bajun <srglvk3@gmail.com>
2015-06-29 20:00:25 +0300

    Create common skeleton for app

--- torrent_try.py
+++ torrent_try.py
@@ -1,5 +1,38 @@
 '''
-Documentation, License etc.
-
-@package torrent_try
+TODO : 	1. Make GUI more beautifull ^^
+	2. Add list of existing torrents from db or filesystem
+	3. Add support of choosing dir fo downloaded file functionality
 '''
+import Tkinter as tk
+import tkFileDialog
+
+import fileoperations as fo
+import maintorrent as mt
+
+class Application():
+    def __init__(self,root):
+      self.root_ = root
+      self.frame_ = self.root_.Frame(width=768, height=576)
+      self.createWidgets()
+      self.frame_.pack()
+      
+      self.root_.mainloop()
+
+    def createWidgets(self):
+      self.frame_.add_button = tk.Button()
+      self.frame_.add_button["text"] = "Add file"
+      self.frame_.add_button["command"] = self.add_file
+      self.frame_.add_button.pack(side="top")
+      
+      self.text_=self.root_.Text(self.frame_)
+      self.text_.pack()
+      
+    def add_file(self):
+      file = tkFileDialog.askopenfile(mode='r')
+      if(file):
+	copied_file = fo.FileOperations().file_copy(file)
+	mt.MainTorrent(self,copied_file)
+      else:
+	self.text_.insert(1.0,'No file added')
+  
+app = Application(tk)
